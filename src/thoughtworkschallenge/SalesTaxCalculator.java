dd05165d9a7896adcd3b2536c90c9129dcb6520f
Pokegear <christoph.ding@gmail.com>
2016-12-14 16:17:41 -0500

    Add sample input files.

--- src/thoughtworkschallenge/SalesTaxCalculator.java
+++ src/thoughtworkschallenge/SalesTaxCalculator.java
@@ -16,6 +16,7 @@ public class SalesTaxCalculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("calculating sales tax...");
     }
     
 }

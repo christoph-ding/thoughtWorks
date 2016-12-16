7de90480ef607f88c8ba5fc6baf59da3dc35aab5
Pokegear <christoph.ding@gmail.com>
2016-12-16 17:14:53 -0500

    Finished code that outputs the correct info.

--- src/thoughtworkschallenge/ParsedItems.java
+++ src/thoughtworkschallenge/ParsedItems.java
@@ -3,11 +3,11 @@ package thoughtworkschallenge;
 import java.math.BigDecimal;
 
 public class ParsedItems {
-    final Float numberBought;
+    final int numberBought;
     final String itemBought;
     final BigDecimal price;
 
-    public ParsedItems(Float numberBought, String itemBought, BigDecimal price) {
+    public ParsedItems(int numberBought, String itemBought, BigDecimal price) {
         this.numberBought = numberBought;
         this.itemBought = itemBought;
         this.price = price;
--- src/thoughtworkschallenge/SalesTaxCalculator.java
+++ src/thoughtworkschallenge/SalesTaxCalculator.java
@@ -18,7 +18,10 @@ public class SalesTaxCalculator {
         FileReader fileReader;
         BufferedReader bufferedReader;
         BigDecimal totalTax;
+        BigDecimal totalSpent;
         String currentLine;
+        String salesTaxMessage;
+        String totalSpentMessage;
         
         // name of the input file
         fileName =  System.getProperty("user.dir") + "/src/thoughtworkschallenge/sample1.txt";       
@@ -26,42 +29,54 @@ public class SalesTaxCalculator {
         try {
             // this is the only value we need to calculate for the output           
             totalTax = new BigDecimal(0.00);
+            totalSpent = new BigDecimal(0.00);
             
             // reading the lines in the input document
             fileReader = new FileReader(fileName);            
             bufferedReader = new BufferedReader(fileReader);                        
-                                                            
+                                      
+            // this prints out the correct line for each item bought
             while((currentLine = bufferedReader.readLine()) != null) {
                 // variable declarations
                 ParsedItems parsedItems;
-                BigDecimal applicableRate;
                 BigDecimal moneySpent;
-//                BigDecimal rate;
-                BigDecimal salesTax;                
-                // exemptItems have a special tax rate
-                // with more time, I would make the exemptItems be an argument to the program
-                List<String> exemptItems = Arrays.asList("book", "headache pills", "chocolate");                
+                BigDecimal applicableRate;                
+                BigDecimal salesTaxPaid;
+                BigDecimal priceAfterTax;
+                // exemptItems have a special tax rate                
+                List<String> exemptItems = Arrays.asList("book", "headache pills", "chocolate");
+                String lineItemMessage;
                                 
                 // get the important information from each line item in the input
-                parsedItems = parseLine(currentLine);
+                parsedItems = parseLine(currentLine);                                
                 
-                System.out.println("doing for: " + parsedItems.itemBought);
-                
+                // this is price of items and number bought
+                moneySpent = parsedItems.price.multiply(BigDecimal.valueOf(parsedItems.numberBought));
+                moneySpent = moneySpent.setScale(2, BigDecimal.ROUND_UP);
+ 
                 // determine the tax rate, based on what the item is                
-                applicableRate = determineTaxRate(parsedItems.itemBought, exemptItems);
+                applicableRate = determineTaxRate(parsedItems.itemBought, exemptItems);                                
+                salesTaxPaid = determineTaxPaid(moneySpent, applicableRate);
+                priceAfterTax = parsedItems.price.add(salesTaxPaid);
+                priceAfterTax = priceAfterTax.setScale(2, BigDecimal.ROUND_UP);
+                                
+                totalTax = totalTax.add(salesTaxPaid);                
+                totalSpent = totalSpent.add(priceAfterTax);
                 
-//                System.out.println("item: " + parsedItems.itemBought + " rate: " + applicableRate);
-                
-//                salesTax = determineTax(moneySpent, rate);
-//                totalTax = totalTax.add(salesTax);                    
-                                                
-                System.out.println("rate for " + parsedItems.itemBought + " tax is: " + applicableRate);
+                // output should be properly formatted;
+                lineItemMessage = parsedItems.numberBought + " " + parsedItems.itemBought + " : " + priceAfterTax;
+                System.out.println(lineItemMessage);
             }            
+            
+            // outputs the lines for total tax, and total money spent after line items
+            salesTaxMessage = "Sales Taxes: " + totalTax;
+            System.out.println(salesTaxMessage);
+            totalSpentMessage = "Total: " + totalSpent;
+            System.out.println(totalSpentMessage);
         }    
 
         catch (FileNotFoundException ex) {
-            System.out.println("problem opening " + fileName);
-//            ex.printStackTrace();
+            System.out.println("problem opening " + fileName);            
         }        
         catch(IOException ex) {
             System.out.println("problem reading " + fileName);
@@ -72,7 +87,7 @@ public class SalesTaxCalculator {
         // variable declarations
         String[] words;
         String[] item;
-        Float numberBought;
+        int numberBought;
         String itemBought;
         BigDecimal price;
         ParsedItems parsedItems;
@@ -82,22 +97,13 @@ public class SalesTaxCalculator {
         item = words[0].split(" ", 2);
         
         // information we need to determine sales tax
-        numberBought = Float.parseFloat(item[0]);
+        numberBought = Integer.parseInt(item[0]);
         itemBought = item[1];
         price = new BigDecimal(Float.parseFloat(words[words.length - 1]));
-        
+                              
         // that information is returned as an object
         parsedItems = new ParsedItems(numberBought, itemBought, price);                               
         return parsedItems;
-    }
-    
-    public static BigDecimal determineTax(BigDecimal price, BigDecimal rate) {        
-        BigDecimal taxPaid;
-        
-        taxPaid = price.multiply(rate);
-        taxPaid = taxPaid.setScale(2, BigDecimal.ROUND_DOWN);
-        
-        return taxPaid;
     }
     
     public static BigDecimal determineTaxRate(String itemBought, List exemptItems) {
@@ -108,20 +114,19 @@ public class SalesTaxCalculator {
             applicableRate = BigDecimal.valueOf(0.15);
         } else if (!itemBought.contains("imported")) {
             Boolean exemptionApplies = false;
-                        
+            
+            // determines if an exemption is found
             for (int i = 0; i < exemptItems.size(); i++) {
                 String exemption;                
                 exemption = (String) exemptItems.get(i);
-                System.out.println("item: " + itemBought + " exempt: " + exemption);            
                 if (itemBought.contains(exemption)) {
-                    System.out.println("exemption found");
                     exemptionApplies = true;
                     break;
                 }                
             }
             
             if (exemptionApplies) {
-                applicableRate = BigDecimal.valueOf(0.05);
+                applicableRate = BigDecimal.valueOf(0.00);
             } else {
                 applicableRate = BigDecimal.valueOf(0.10);    
             }                                    
@@ -130,8 +135,16 @@ public class SalesTaxCalculator {
           // for edge cases
             applicableRate = BigDecimal.valueOf(0.10);
         }
-                
-        return applicableRate;
         
+        return applicableRate;        
+    }
+    
+    public static BigDecimal determineTaxPaid(BigDecimal price, BigDecimal rate) {        
+        BigDecimal taxPaid;
+        
+        taxPaid = price.multiply(rate);
+        taxPaid = taxPaid.setScale(2, BigDecimal.ROUND_UP);
+        
+        return taxPaid;
     }
 }
